446e029e35f91cbed8c015531d87dd33cc3dbed6
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-09-26 23:28:48 -0400

    Styled components

--- router-project/src/components/Navbar.js
+++ router-project/src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavUnlisted = styled.ul`
+  text-decoration: none;  
+`;
+
+const StyledLink = styled(Link)`
+  color: Blue;
+  text-decoration: none;
+  margin: 1rem;
+  position: relative;
+
+  :hover{
+    color: red;
+  }
+`;
 
 export default function Navbar() {
   return (
-    <ul>
-        <li><Link to="/" >Home </Link></li>
-        <li><Link to="about" >About </Link></li>
-        <li><Link to="projects">Projects </Link></li>
-    </ul>
+    <NavUnlisted>
+        <StyledLink to="/">Home </StyledLink>
+        <StyledLink to="about">About </StyledLink>
+        <StyledLink to="projects">Projects </StyledLink>
+    </NavUnlisted>
   )
 }
