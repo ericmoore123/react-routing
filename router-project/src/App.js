12aaa5f50c73ba9592714edd61ec522759f2f495
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2022-09-27 23:29:36 -0400

    Added react-modal to function as a global state setter

--- router-project/src/App.js
+++ router-project/src/App.js
@@ -1,29 +1,70 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Navbar from './components/Navbar';
 
-export const GlobalContext = React.createContext();
+// React-Modal
+import Modal from "react-modal";
+
+export const GlobalContext = React.createContext('');
 
 const App = () => {
 
   const [globalData, setGlobalData] = useState('Test!');
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsOpen(false);
+    localStorage.setItem('nameSet', true);
+    localStorage.setItem('userName', globalData);
+  };
+
+  useEffect(() => {
+    console.log('Ran useEffect');
+    const nameStatus = localStorage.getItem('nameSet');
+    const name = localStorage.getItem('userName');
+    if(nameStatus){
+      setIsOpen(true);
+      setGlobalData(name);
+    }else{
+      setIsOpen(true);
+    };
+  }, []);
 
   return (
     <div className="App">
-      <Navbar />
       <GlobalContext.Provider value={{ globalData, setGlobalData }}>
+        <Navbar />
+        <Modal
+          className="mymodal"
+          isOpen={isOpen}
+          onRequestClose={closeModal}
+          ariaHideApp={false}
+          contentLabel="My dialog"
+          style={{
+            overlay: {
+              backgroundColor: 'rgb(238, 238, 238)'
+            }
+          }}
+        >
+          <form>
+            <h3 htmlFor="name">What is your name?</h3>
+            <input type="text" id="name" onChange={(e) => setGlobalData(e.target.value)}/>
+            <button className="closeModal" onClick={closeModal}>Close</button>
+          </form>
+        </Modal>
+
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/Projects" element={<Projects/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Projects" element={<Projects />} />
         </Routes>
       </GlobalContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
